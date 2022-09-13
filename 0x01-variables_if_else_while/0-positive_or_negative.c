53d6e4b4088cbea0b6ebc404271f3e916f0aac41
Anthonia Anyanwu <anyanwuanthonia@gmail.com>
2022-09-12 21:00:37 -0700

    started

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,26 @@
 
 /**
  * main - Entry point
- * Return: 0
+ *
+ * Return: return one of the 3 options
  */
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0);
+	else if (n == 0)
 	{
-	printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
