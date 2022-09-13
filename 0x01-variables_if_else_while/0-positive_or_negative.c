e16be11075e26e15d4237e690320d23eea81ccf5
Anthonia Anyanwu <anyanwuanthonia@gmail.com>
2022-09-12 20:13:20 -0700

    we started c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,21 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
- * Return: return one of the 3 options
+ * Return: 0
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;n = rand()
-		if (n > 0)
-		{
-			printf("%d is positive\n", n);
-		}
-		else if (n == 0)
-		{
-			printf("%d is zero\n", n);
-		}
-		else
-		{
-			printf("%d is negative\n", n);
-		}
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0);
+	printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
 }
-return (0);
-
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-Positive anything is better than negative nothing
+program will assign a random number to the variable
