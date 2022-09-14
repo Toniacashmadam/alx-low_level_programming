6fcdfed4e9d7fe46fa1a8dedef0e4ee9d65775e8
Anthonia Anyanwu <anyanwuanthonia@gmail.com>
2022-09-13 18:55:19 -0700

    started code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char c, b;
+for (b = 0; b <= 9; b++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - funtion to print lowercase character
+ * @c: is the int that will be use for the argument
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}
