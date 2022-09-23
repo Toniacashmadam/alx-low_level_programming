ce34e07fb6ff30a0a2af3cb8ca3ce54873a63c58
Anthonia Anyanwu <anyanwuanthonia@gmail.com>
2022-09-22 19:24:03 -0700

    correction for last task 102-magic.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -28,14 +28,18 @@ else
 putchar('.');
 }
 }
+
 /**
  * print_buffer - prints a buffer
  * @b: buffer to print
  * @size: size of buffer
  * Return:void
+ *
  */
 void print_buffer(char *b, int size)
+{
 int i;
+
 for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
 printf("%08x: ", i * 10);
