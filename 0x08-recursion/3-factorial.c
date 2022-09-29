c1e53958305e992d54a9a846c3ee489a81bf7116
Anthonia Anyanwu <anyanwuanthonia@gmail.com>
2022-09-28 18:35:40 -0700

    correction

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,19 +4,19 @@
  * @n: parameter
  * Return: end program
  */
-
 int factorial(int n)
 {
 if (n < 0)
 {
 return (-1);
+}
 else if (n == 0)
 {
 return (1);
 }
 else
 {
-n = n * factorial(n - 1);
+n = n *factorial(n - 1);
 return (n);
 }
 return (0);
